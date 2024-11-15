c041232092446468afa49699a185a91da773a781
Arkhalisal <wf51182474@gmail.com>
2024-11-15 10:43:53 +0800

    cancel battle 21

--- app/battle21/page.js
+++ app/battle21/page.js
@@ -1,37 +1,37 @@
-"use client";
+// "use client";
 
-import Cookies from "universal-cookie";
-import { useState, useEffect } from "react";
+// import Cookies from "universal-cookie";
+// import { useState, useEffect } from "react";
 
-const cookies = new Cookies();
+// const cookies = new Cookies();
 
-export default function Page() {
-  const [playerName, setPlayerName] = useState("");
+// export default function Page() {
+//   const [playerName, setPlayerName] = useState("");
 
-  useEffect(() => {
-    const name = cookies.get("userName");
-    if (name) {
-      setPlayerName(name);
-    }
-  }, []);
+//   useEffect(() => {
+//     const name = cookies.get("userName");
+//     if (name) {
+//       setPlayerName(name);
+//     }
+//   }, []);
 
-  return (
-    <div className="flex flex-col items-center m-6">
-      <CreateRoom playerName={playerName} />
-      <JoinRoom />
-      <div>Current Name: {playerName}</div>
-      <input
-        value={playerName}
-        onChange={(ev) => {
-          setPlayerName(ev.target.value);
-          cookies.set("userName", ev.target.value);
-        }}
-      ></input>
-      <div className="decoration-neutral-900">
-        <a href="./" className="">
-          返回
-        </a>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="flex flex-col items-center m-6">
+//       <CreateRoom playerName={playerName} />
+//       <JoinRoom />
+//       <div>Current Name: {playerName}</div>
+//       <input
+//         value={playerName}
+//         onChange={(ev) => {
+//           setPlayerName(ev.target.value);
+//           cookies.set("userName", ev.target.value);
+//         }}
+//       ></input>
+//       <div className="decoration-neutral-900">
+//         <a href="./" className="">
+//           返回
+//         </a>
+//       </div>
+//     </div>
+//   );
+// }
